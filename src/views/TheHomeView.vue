84b884a5a5c8d98521e67bb654cd98858dbe69c5
Sylnavy <valmymabika@gmail.com>
2023-04-05 11:51:23 +0100

    installation of packages

---
+++ src/views/TheHomeView.vue
@@ -0,0 +1,17 @@
+<template lang="ts">
+  <div>
+    <TheHomePage></TheHomePage>
+  </div>
+</template>
+<script>
+import TheHomePage from '../components/TheHomePage.vue'
+export default {
+  name:TheHomeView,
+  components:{TheHomePage}
+
+  
+}
+</script>
+<style lang="ts">
+  
+</style>
